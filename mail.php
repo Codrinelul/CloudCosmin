2e5db4c79ca313f8065e84705a626e45a1b9ec85
Squirtle420 <sandu.codrin2002@gmail.com>
2021-08-06 03:00:40 +0300

    mesaj

--- mail.php
+++ mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "clodydesigniasi@gmail.com";
+        $recipient = "sandu.codrin2002@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
