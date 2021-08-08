8e57b7cd6812e51875f0429b5f4c26988ccd6dc5
Squirtle420 <sandu.codrin2002@gmail.com>
2021-08-09 00:24:37 +0300

    form2

--- js/ajax-form.js
+++ js/ajax-form.js
@@ -3,7 +3,7 @@ function sendContact() {
     valid = validateContact();
     if (valid) {
         jQuery.ajax({
-            url: "contact_mail.php",
+            url: "mail.php",
             data: 'userName=' + $("#userName").val() + '&userEmail=' +
                 $("#userEmail").val() + '&subject=' +
                 $("#subject").val() + '&content=' +
